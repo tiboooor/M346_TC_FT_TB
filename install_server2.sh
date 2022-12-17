8f3e9bf1122b8f641f1a563650fcff93e49aa0a1
franzi166 <98404619+franzi166@users.noreply.github.com>
2022-12-17 19:46:13 +0100

    Update install_server2.sh

--- install_server2.sh
+++ install_server2.sh
@@ -4,12 +4,12 @@ SUBNET_ID=$(aws ec2 describe-subnets --query "Subnets[?CidrBlock=='172.31.0.0/20
 
 # key erstellen
 aws ec2 create-key-pair --key-name cms_key --key-type rsa --query 'KeyMaterial' --output text > ~/.ssh/cms_key.pem
-# security group 1 erstellen
-aws ec2 create-security-group --group-name  cms-web-group --description "SSH and HTTP"
-# security group 1 auf Ports autorisieren
-aws ec2 authorize-security-group-ingress --group-name cms-sec-group --protocol tcp --port 80 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-name cms-sec-group --protocol tcp --port 22 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-name cms-sec-group --protocol tcp --port 3306 --cidr 0.0.0.0/0
+# # security group 1 erstellen
+# aws ec2 create-security-group --group-name  cms-web-group --description "SSH and HTTP"
+# # security group 1 auf Ports autorisieren
+# aws ec2 authorize-security-group-ingress --group-name cms-sec-group --protocol tcp --port 80 --cidr 0.0.0.0/0
+# aws ec2 authorize-security-group-ingress --group-name cms-sec-group --protocol tcp --port 22 --cidr 0.0.0.0/0
+# aws ec2 authorize-security-group-ingress --group-name cms-sec-group --protocol tcp --port 3306 --cidr 0.0.0.0/0
 
 
 # direcory für inital datei von webserver
@@ -19,7 +19,7 @@ cd ~/ec2cmsdbserver
 touch initial.txt
 echo "#!/bin/bash\nsudo apt-get update\nsudo apt-get -y install mariadb-server\nsudo systemctl start mariadb.service" > initial.txt
 # erstellen von EC2 instances
-aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --security-groups cms-sec-group --subnet-id $SUBNET_ID --private-ip-address 172.31.0.100 --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_dataserver}]'
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --subnet-id $SUBNET_ID --private-ip-address 172.31.0.100 --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_dataserver}]'
 
 # direcory für inital datei von webserver
 mkdir ~/ec2cmswebserver
@@ -28,7 +28,7 @@ cd ~/ec2cmswebserver
 touch initial.txt
 echo "#!/bin/bash\nsudo apt-get update\nsudo apt-get -y install apache2" > initial.txt
 # erstellen von EC2 instance
-aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --security-groups cms-sec-group --subnet-id $SUBNET_ID --private-ip-address 172.31.0.200 --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_webserver}]'
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --subnet-id $SUBNET_ID --private-ip-address 172.31.0.200 --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_webserver}]'
 
 chmod 600 ~/.ssh/cms_key.pem
 
