cfeb76df5f9c7e7d1e10ef95917a308cf8f0b29d
franzi166 <98404619+franzi166@users.noreply.github.com>
2022-12-18 21:29:41 +0100

    neue scripte

--- install_server.sh
+++ install_server.sh
@@ -11,8 +11,28 @@ aws ec2 attach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc_id
 
 aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.1.0/24 --region "us-east-1" --availability-zone "us-east-1e" --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=subnet_cms_webserver}]'
 subnet_id=$(aws ec2 describe-subnets --filters Name=tag:Name,Values=subnet_cms_webserver --query 'Subnets[*].SubnetId' --output text)
-#route table weggelassen vorerst
-#aws ec2 create-route-table --vpc-id $vpc_id --region "us-east-1" 
+
+# Create the NACL
+aws ec2 create-network-acl --vpc-id $vpc_id -subnet-id $subnet_id
+# Get the ID of the NACL
+
+NACL_ID=$(aws ec2 describe-network-acls --filters Name=vpc-id,Values=$vpc_id --query 'NetworkAcls[0].NetworkAclId' --output text)
+aws ec2 delete-network-acl-entry --network-acl-id $NACL_ID --ingress --rule-number *
+aws ec2 delete-network-acl-entry --network-acl-id $NACL_ID --egress --rule-number *
+
+# Create an ingress rule that allows incoming traffic from a specific IP address on a specific port
+aws ec2 create-network-acl-entry --network-acl-id $NACL_ID --ingress --rule-number 100 --protocol all --port-range From=0,To=65535 --cidr-block 0.0.0.0/0 --rule-action allow
+# Create an egress rule that allows outgoing traffic to a specific IP address on a specific port
+aws ec2 create-network-acl-entry --network-acl-id $NACL_ID --egress --rule-number 100 --protocol all --port-range From=0,To=65535 --cidr-block 0.0.0.0/0 --rule-action allow
+
+
+#route table
+aws ec2 create-route-table --vpc-id $vpc_id --region "us-east-1" --tag-specifications 'ResourceType=route-table,Tags=[{Key=Name,Value=cms_route_table}]'
+
+route_id=$(aws ec2 describe-route-tables --filters Name=tag:Name,Values=cms_route_table --query 'RouteTables[0].RouteTableId' --output text)
+
+aws ec2 create-route --route-table-id $route_id --destination-cidr-block 0.0.0.0/0 --gateway-id $igw_id
+
 # key erstellen
 aws ec2 create-key-pair --key-name cms_key --key-type rsa --query 'KeyMaterial' --output text > ~/.ssh/cms_key.pem
 # security group  erstellen
@@ -23,6 +43,7 @@ sec_id=$(aws ec2 describe-security-groups --filters "Name=vpc-id,Values=$vpc_id"
 aws ec2 authorize-security-group-ingress --group-id $sec_id --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sec_id --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $sec_id --protocol tcp --port 3306 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $sec_id --protocol icmp --port -1 --cidr 0.0.0.0/0
 
 
 # direcory für inital datei von webserver
@@ -30,17 +51,17 @@ mkdir ~/ec2cmsdbserver
 cd ~/ec2cmsdbserver
 # inital datei für dbinstallation
 touch initial.txt
-echo "#!/bin/bash\nsudo apt-get update\nsudo apt-get -y install mariadb-server\nsudo systemctl start mariadb.service" > initial.txt
+echo "#!/bin/bash\nsudo apt-get update\nsudo apt-get -y install mariadb-server\nsudo systemctl start mariadb.service\nsudo apt install openssh-server\nsudo systemctl enable ssh" > initial.txt
 # erstellen von EC2 instances
-aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --private-ip-address 10.0.1.20 --subnet-id $subnet_id --security-group-ids $sec_id --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_dataserver}]'
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --associate-public-ip-address --private-ip-address 10.0.1.20 --subnet-id $subnet_id --security-group-ids $sec_id --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_dataserver}]'
 
 # direcory für inital datei von webserver
 mkdir ~/ec2cmswebserver
 cd ~/ec2cmswebserver
 # inital datei für webserverinstallation
 touch initial.txt
-echo "#!/bin/bash\nsudo apt-get update\nsudo apt-get -y install apache2" > initial.txt
+echo "#!/bin/bash\nsudo apt-get update\nsudo apt-get -y install apache2\nsudo apt install openssh-server\nsudo systemctl enable ssh" > initial.txt
 # erstellen von EC2 instance
-aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --private-ip-address 10.0.1.10 --subnet-id $subnet_id --security-group-ids $sec_id --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_webserver}]'
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --associate-public-ip-address --private-ip-address 10.0.1.10 --subnet-id $subnet_id --security-group-ids $sec_id --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_webserver}]'
 
 chmod 600 ~/.ssh/cms_key.pem
--- install_server3.sh
+++ install_server3.sh
@@ -1,1 +1,38 @@
+#!/bin/bash
 
+# key erstellen
+aws ec2 create-key-pair --key-name cms_key --key-type rsa --query 'KeyMaterial' --output text > ~/.ssh/cms_key.pem
+# security group  erstellen
+aws ec2 create-security-group --group-name sec-group-cms --description "SSH and HTTP and 3306"
+sec_id=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=sec-group-cms" --query 'SecurityGroups[*].{ID:GroupId}' --output text)
+
+# security group  auf Ports autorisieren
+aws ec2 authorize-security-group-ingress --group-id $sec_id --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $sec_id --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $sec_id --protocol tcp --port 3306 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $sec_id --protocol icmp --port -1 --cidr 0.0.0.0/0
+
+
+# direcory für inital datei von webserver
+mkdir ~/ec2cmsdbserver
+cd ~/ec2cmsdbserver
+# inital datei für dbinstallation
+touch initial.txt
+echo "#!/bin/bash\nsudo apt-get update\nsudo apt-get -y install mariadb-server\nsudo systemctl start mariadb.service" > initial.txt
+# erstellen von EC2 instances
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --security-group-ids $sec_id --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_dataserver}]'
+INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=cms_dataserver" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+
+# direcory für inital datei von webserver
+mkdir ~/ec2cmswebserver
+cd ~/ec2cmswebserver
+# inital datei für webserverinstallation
+touch initial.txt
+echo "#!/bin/bash\nsudo apt-get update\nsudo apt-get -y install apache2" > initial.txt
+# erstellen von EC2 instance
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name cms_key --security-group-ids $sec_id --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=cms_webserver}]'
+INSTANCE_ID2=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=cms_webserver" --query 'Reservations[*].Instances[*].InstanceId' --output text)
+PUBLIC_IP2=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID2 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+
+chmod 600 ~/.ssh/cms_key.pem
